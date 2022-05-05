4579ff39d5ae6d3d3c021261b94cebee83f96224
Kovalskiii <49062638+Kovalskiii@users.noreply.github.com>
2022-05-05 17:31:58 +0300

    cors fix

--- src/modules/core/cors.js
+++ src/modules/core/cors.js
@@ -1,17 +1,16 @@
-// export default function cors(app) {
-//   app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header(
-//       'Access-Control-Allow-Headers',
-//       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-//     );
-//     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-//     next();
-//   });
-// }
-
-import cors from "cors";
-
 export default function corsExpress(app) {
-  app.use(cors());
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    );
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    next();
+  });
 }
+
+// import cors from "cors";
+// export default function corsExpress(app) {
+//   app.use(cors());
+// }
