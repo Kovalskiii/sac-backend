07ce2e9a0b2638ef009d5a3e1cac8c7ccc5e0ebb
Kovalskiii <49062638+Kovalskiii@users.noreply.github.com>
2022-05-06 15:52:50 +0300

    photo upload fix

--- src/modules/worker/queries/uploadPhotoQuery.js
+++ src/modules/worker/queries/uploadPhotoQuery.js
@@ -9,10 +9,12 @@ const uploadPhotoQuery = async (photoFile, workerId, res, updatedWorkerObj, oper
   const workerDocRef = doc(db, 'workers', workerId);
 
   if (photoFile) {
-    const photoName = workerId;
+    const fileExtension = photoFile.mimetype.split('/').pop();
+    const photoName = workerId + "." + fileExtension;
     const metadata = { contentType: photoFile.mimetype };
-    const storageRef = ref(storage, photoName);
+    const storageRef = ref(storage, `${photoName}`);
     const uploadTask = uploadBytesResumable(storageRef, photoFile.buffer, metadata);
+    console.log(photoFile)
 
     await uploadTask.on('state_changed',
       (snapshot) => {},
